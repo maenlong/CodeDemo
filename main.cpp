e18e3f0a4956c0e45f71bab06239bc8a77283586
mael <15801230835@163.com>
2017-11-25 09:53:18 +0800

    Test Push

--- main.cpp
+++ main.cpp
@@ -9,11 +9,11 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-//    ListViewDemo wgt;
-//    wgt.show();
+    ListViewDemo wgt;
+    wgt.show();
 
-    PSelectDateView x;
-    x.show();
+//    PSelectDateView x;
+//    x.show();
 
     return a.exec();
 }
